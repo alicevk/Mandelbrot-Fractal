aa39f79dc50effa129f1326cec2c749b465898ba
Alice Vitti Kageyama Ferreira <alicevkf@gmail.com>
2023-10-06 21:06:42 -0300

    Adds version 2B :)

--- ver-2.py
+++ ver-2.py
@@ -10,16 +10,16 @@ import matplotlib.pyplot as plt
 #                                  PARAMETERS                                  #
 # ---------------------------------------------------------------------------- #
 
-quality = 1e4 # Related to pixel density, image quality and size
-iterations = 1e4 # Related to precision, number of iterations
+quality = 1e3 # Related to pixel density, image quality and size
+iterations = 1e3 # Related to precision, number of iterations
 
 
 # ---------------------------------------------------------------------------- #
 #                                   FUNCTIONS                                  #
 # ---------------------------------------------------------------------------- #
 
-# Mandelbrot plot function
-def Mandelbrot(quality, iterations):
+# Mandelbrot plot function :)
+def Mandelbrot(quality, iterations, ver='A'):
     quality, iterations = int(quality), int(iterations)
     
     # Complex plane mesh array
@@ -30,12 +30,25 @@ def Mandelbrot(quality, iterations):
 
     # Mandelbrot check
     nums = np.zeros(quality)
-    for current in range(iterations):
-        nums = (nums**2)+mesh
-        nums = (np.abs(nums)<2)*nums
-        
-        # Anxiety reliever :)
-        print(f'\nProcessing: {round(current/iterations*100, 2)}% done.')
+    
+    # (Version A)
+    if ver == 'A':
+        for current in range(iterations):
+            nums = (nums**2)+mesh
+            nums = (np.abs(nums)<2)*nums
+            
+            # Anxiety reliever :)
+            print(f'\nProcessing: {round(current/iterations*100, 2)}% done.')
+    
+    # (Version B)
+    elif ver == 'B':
+        for current in range(iterations):
+            nums = (((np.abs(nums)<2)*nums)**2)+mesh
+            
+            # Anxiety reliever :)
+            print(f'\nProcessing: {round(current/iterations*100, 2)}% done.')
+            
+    else: print(f'Version {ver} not found.')
 
     # Graph plot
     abs = np.abs(nums)
@@ -44,7 +57,7 @@ def Mandelbrot(quality, iterations):
     plt.xticks([])
     plt.yticks([])
 
-    plt.savefig(f'./figs/v2-qual{int(quality)}_iter{int(iterations)}.png')
+    plt.savefig(f'./figs/v2{ver}-qual{int(quality)}_iter{int(iterations)}.png')
     plt.show()
 
 
@@ -53,6 +66,7 @@ def Mandelbrot(quality, iterations):
 # ---------------------------------------------------------------------------- #
 
 Mandelbrot(quality, iterations)
+
 
 # ----------------------------------- TO DO ---------------------------------- #
 # coloring
